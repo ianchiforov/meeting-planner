835cae3b08b9a3e65039be0b669ad6c880459ada
ianchiforov <ianchiforov@softjourn.com>
2022-08-25 12:54:32 +0300

    welcome, date and about views

--- meeting_planner/my_meeting_planner_app/views.py
+++ meeting_planner/my_meeting_planner_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+
+def welcome(request):
+    return HttpResponse("Welcome to my meeting planner!")
+
+
+def date(request):
+    return HttpResponse(f"This page was served at {datetime.now()}")
+
+
+def about(request):
+    return HttpResponse("This is about page for my meeting planner...")
